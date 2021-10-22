0767eb9a387f1bb9b3e3be1941d31c8835d471ae
Carlos Osorio <hacybeyker@gmail.com>
2021-10-21 19:17:04 -0500

    se modifico el proyecto para AfoxPlus

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,12 +1,12 @@
 object ConfigureApp {
     //TODO change urlRepoDependencies with your urlRepoDependencies name
-    const val urlRepoDependencies = "https://maven.pkg.github.com/Hacybeyker/app-android-module"
+    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-android-module"
     //TODO change organization with your organization name
-    const val organization = "hacybeyker"
+    const val organization = "afoxplus"
     //TODO change applicationId with your module name
     const val applicationId = "app-android-module"
     //TODO change groupId with your groupId name
-    const val groupId = "com.hacybeyker.android"
+    const val groupId = "com.afoxplus.android"
     //TODO change artifactId with your module name
     const val artifactId = "module"
     const val version = "1.0.0"
