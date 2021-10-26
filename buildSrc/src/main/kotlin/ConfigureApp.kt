8ec0e234b1884f56c00b0c8bce099f80caa6e8de
Carlos Osorio <hacybeyker@gmail.com>
2021-10-26 07:43:02 -0500

    se modificaron las configuraciones

--- buildSrc/src/main/kotlin/ConfigureApp.kt
+++ buildSrc/src/main/kotlin/ConfigureApp.kt
@@ -1,10 +1,12 @@
 object ConfigureApp {
     //TODO change urlRepoDependencies with your urlRepoDependencies name
-    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/app-android-module"
+    const val urlRepoDependencies = "https://maven.pkg.github.com/afoxplus/your_path_module"
     //TODO change organization with your organization name
     const val organization = "afoxplus"
+    //TODO change organization with your organization name
+    const val projectName = "app-android-module"
     //TODO change applicationId with your module name
-    const val applicationId = "app-android-module"
+    const val applicationId = "app.android.module"
     //TODO change groupId with your groupId name
     const val groupId = "com.afoxplus.android"
     //TODO change artifactId with your module name
--- module/src/androidTest/java/com/afoxplus/module/ExampleInstrumentedTest.kt
+++ module/src/androidTest/java/com/afoxplus/module/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hacybeyker.module.test", appContext.packageName)
+        assertEquals("com.afoxplus.module.test", appContext.packageName)
     }
 }
